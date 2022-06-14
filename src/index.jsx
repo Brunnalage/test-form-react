81be9bf337c0f024e1d09ce5c86c7ab86f2e4f34
Brunnalage <brunnalage@gmail.com>
2022-06-14 18:26:49 -0300

    Feat: import 'GlobalStyle in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-
+import {Home} from './pages/Home'
+import {GlobalStyle} from './pages/Home'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>    
+    <GlobalStyle /> 
+       <Home />
   </React.StrictMode>
 )
