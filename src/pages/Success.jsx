9ffec01be36ff345ad37cf35f6209e3b3096ed93
Brunnalage <brunnalage@gmail.com>
2022-06-16 13:08:31 -0300

    Style: Grid definition for header and button

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -2,13 +2,33 @@ import styled from 'styled-components';
 import React from 'react';
 import Box from '../components/Box';
 import { Header } from '../components/header/header'; 
+import { ButtonLarge } from '../components/ButtonLarge';
 
+const HeaderSuccess = styled.div`
+ grid-area: header;
+    display: grid;
+    grid-gap: 3em;
+    height: 50%;       
+    align-content: center;    
+`;
 
+const MainSuccess = styled.div`
+grid-area: main;
+    height: 45%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
 
 export function Success(){
 return(
     <Box>
+        <HeaderSuccess>
       <Header title="Success!"/>
+      </HeaderSuccess>
+      <MainSuccess>
+      <ButtonLarge title="Go Back!"/>
+      </MainSuccess>
     </Box>
 )
 }
