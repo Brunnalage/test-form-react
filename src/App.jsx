1bacb2a08cf5588edd0e3eb122881b1a81f59027
Brunnalage <brunnalage@gmail.com>
2022-06-16 12:30:55 -0300

    Refactor: Transferring background style from application to App file

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import {Home} from './pages/Home'
+import { createGlobalStyle } from 'styled-components'
 
+export const GlobalStyle = createGlobalStyle`
+   html, body{ box-sizing: border-box;
+    margin: 0;
+    padding: 0;background: #E5E5E5;
+    font-family: 'Nunito', sans-serif;
+    height: 100vh;
+  }
+   `
+
+
+export function App (){
+    return(
+        <>
+        <GlobalStyle />
+        <Home />
+        </>
+    )
+}
