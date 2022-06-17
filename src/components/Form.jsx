4d0193bf64c93722cdcfee9b3ce884e5e233a87a
Brunnalage <brunnalage@gmail.com>
2022-06-16 23:05:15 -0300

    Feat: Add full name validation on the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -15,33 +15,52 @@ const FormRegister = styled.form`
     flex-wrap: wrap;   
     `;
 
-export const Form= () => {
+export const Form = () => {
     const [name, setName] = React.useState('');
     const [email, setEmail] = React.useState('');
     const [phone, setPhone] = React.useState('');
     const [password, setPassword] = React.useState('');
     const [birthday, setBirthday] = React.useState('');
+    const [error, setError] = React.useState(null);
 
-    
-      function handleSubmit(event) {
+
+    function validateName(value) {
+        if (!/(^[A-Za-z]{4,})([ ]{1})([A-Za-z])/.test(value)) {
+            setError('Full name invalid.')
+            return false
+        } else
+            setError(null);
+        return true;
+    }
+
+    function handleBlur({ target }) {
+       validateName(target.value)     
+    }
+
+    function handleChange ({ target}){
+        if (error) validateName(target.value);
+        setName(target.value)
+    }
+
+    function handleSubmit(event) {
         event.preventDefault();
-        console.log(event);
-      }
-return (
-    <FormRegister onSubmit = {handleSubmit}>     
-    <InputField value={name} placeholder="Name" label="Full Name*" type="text" onChange={(event) => setName(event.target.value)}/>  
-    <InputField value={email} placeholder="Email" label="Email*" type="email" onChange={(event) => setEmail(event.target.value)}/>
-    <InputField value={phone} placeholder="Phone" label="Phone*" type="tel"onChange={(event) => setPhone(event.target.value)}/>
-    <InputField value={password} placeholder="Password" label="Password*" type="password" onChange={(event) => setPassword(event.target.value)}/>
-    <InputField value={birthday} placeholder="Birthdate" label="Birthdate*" type="date" onChange={(event) => setBirthday(event.target.value)}/>
-    <Checkbox />
-    <Button title="Register"/>
- </FormRegister>
-)
+        if (validateName(name)){
+            console.log('enviou')
+        }else{
+        console.log('naoenviar')
+    }
+    }
+    return (
+        <FormRegister onSubmit={handleSubmit}>
+            <InputField value={name} placeholder="Name" label="Full Name*" type="text" onChange={handleChange} onBlur={handleBlur} errorMessage= {error && <p>{error}</p>} />
+           
+            <InputField value={email} placeholder="Email" label="Email*" type="email" onChange={(event) => setEmail(event.target.value)} />
+            <InputField value={phone} placeholder="Phone" label="Phone*" type="tel" onChange={(event) => setPhone(event.target.value)} />
+            <InputField value={password} placeholder="Password" label="Password*" type="password" onChange={(event) => setPassword(event.target.value)} />
+            <InputField value={birthday} placeholder="Birthdate" label="Birthdate*" type="date" onChange={(event) => setBirthday(event.target.value)} />
+            <Checkbox />
+            <Button title="Register" />
+        </FormRegister>
+    )
 
 }
-
-/*    const handleChange = (e) => {
-        const { value } = e.target;
-        onChange = { value };
-      }*/
