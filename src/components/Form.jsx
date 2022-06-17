70a1318ee806d7178fb29acab94b84ad0f4df269
Brunnalage <brunnalage@gmail.com>
2022-06-16 21:11:45 -0300

    Refactor:  Adjusting inputs so they are reactive

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,6 +16,11 @@ const FormRegister = styled.form`
     `;
 
 export const Form= () => {
+    const [name, setName] = React.useState('');
+    const [email, setEmail] = React.useState('');
+    const [phone, setPhone] = React.useState('');
+    const [password, setPassword] = React.useState('');
+    const [birthday, setBirthday] = React.useState('');
 
     
       function handleSubmit(event) {
@@ -24,11 +29,11 @@ export const Form= () => {
       }
 return (
     <FormRegister onSubmit = {handleSubmit}>     
-    <InputField id="name" name="" placeholder="Name" label="Full Name*" type="text"/>
-    <InputField placeholder="Email" label="Email*" type="email"/>
-    <InputField placeholder="Phone" label="Phone" type="tel"/>
-    <InputField placeholder="Password" label="Password*" type="password"/>
-    <InputField placeholder="Birthdate" label="Birthdate*" type="date"/>
+    <InputField value={name} placeholder="Name" label="Full Name*" type="text" onChange={(event) => setName(event.target.value)}/>  
+    <InputField value={email} placeholder="Email" label="Email*" type="email" onChange={(event) => setEmail(event.target.value)}/>
+    <InputField value={phone} placeholder="Phone" label="Phone*" type="tel"onChange={(event) => setPhone(event.target.value)}/>
+    <InputField value={password} placeholder="Password" label="Password*" type="password" onChange={(event) => setPassword(event.target.value)}/>
+    <InputField value={birthday} placeholder="Birthdate" label="Birthdate*" type="date" onChange={(event) => setBirthday(event.target.value)}/>
     <Checkbox />
     <Button title="Register"/>
  </FormRegister>
