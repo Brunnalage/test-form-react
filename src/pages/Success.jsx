6780e9c87d3b79df41ddcaa5921a9328db79c400
Brunnalage <brunnalage@gmail.com>
2022-06-18 18:06:49 -0300

    Feat: Creating a styled.js file for the 'success' page with the styling content that was on the page

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,16 +1,9 @@
-import styled from 'styled-components';
+
 import React from 'react';
 import Box from '../components/Box/Box';
 import { Header } from '../components/Header/Header'; 
 import  Button  from '../components/Button/Button';
-
-const MainSuccess = styled.div`
-grid-area: main;
-    height: 45%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
+import { MainSuccess } from './Success.styled';
 
 export function Success(){
 return(
---
+++ src/pages/Success.styled.js
@@ -0,0 +1,10 @@
+
+import styled from 'styled-components';
+
+export const MainSuccess = styled.div`
+grid-area: main;
+    height: 45%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
