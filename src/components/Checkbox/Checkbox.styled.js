4dab8036aa22bc9086ca0054f8db643be969ae03
Brunnalage <brunnalage@gmail.com>
2022-06-19 19:28:19 -0300

    Style:  Adding style to the checkbox

--- src/components/Checkbox/Checkbox.styled.js
+++ src/components/Checkbox/Checkbox.styled.js
@@ -5,48 +5,17 @@ export const CheckboxContainer = styled.div`
   font-size: 1em; 
   text-shadow: 1px 4px 4px rgba(0, 0, 0, 0.25);    
   color:#111111;       
-  align-self: center;           
+  align-self: center;     
+  align-items: center;      
 `;
 
-export const Checkboxlabel = styled.label`
-  display:flex;        
-`;
-
-export const HiddenCheckbox = styled.input.attrs({ type: 'checkbox' })`
-  overflow: hidden; 
-  white-space: nowrap;width: 1px;
-  height: 1px;
-  margin: -2px;
-  padding: 0;   
-`;
-
-export const Text = styled.label`
-  display:flex;
-cursor: pointer; 
-`;
-
-export const StyledCheckbox = styled.label` 
-  display: flex;
-  width: 20px;
-  height: 20px;
-  border-radius:2px;  
-  margin-right: 6px;  
-  background:#AAAAAA; 
-  justify-content: center;
-  align-items: center;
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
-  cursor: pointer;
-  img {
-    display: ${props => props.checked ? 'flex' : 'none'};
-    background-color: #0DBDBD;
-  }   
-`;
-
-export const Check = styled.img`
-  width: 20px;
-  border-radius:2px;
-`;
-
+export const Checkmark = styled.input`
+width: 20px;
+height: 20px;
+border-radius:2px;  
+accent-color: rgb(0, 164, 180);
+box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`
 export const Error = styled.p`
   display: block;
   color: #ff8e8e;
--- src/components/Checkbox/checkbox.jsx
+++ src/components/Checkbox/checkbox.jsx
@@ -1,21 +1,16 @@
 import React from "react";
-import CheckIcon from "../../img/checkIcon.svg"
 import { CheckboxContainer } from "./Checkbox.styled";
-import { Checkboxlabel } from "./Checkbox.styled";
-import { StyledCheckbox } from "./Checkbox.styled";
-import { HiddenCheckbox } from "./Checkbox.styled";
-import { Check } from "./Checkbox.styled";
-import { Text } from "./Checkbox.styled";
 import { Error } from "./Checkbox.styled";
 import { FormInfo } from "../Inputs/Inputs.styled";
+import { Checkmark } from "./Checkbox.styled";
 
 
 const Checkbox = ({ type, onChange, checked, error, onBlur }) => {
 
     return (
         <FormInfo>
-            <CheckboxContainer>
-                 <input type={type} onChange={onChange} checked={checked} onBlur={onBlur}/>I accept the terms and privacy                 
+            <CheckboxContainer>      
+            <Checkmark type={type} onChange={onChange} checked={checked} onBlur={onBlur}/>I accept the terms and privacy         
             </CheckboxContainer>            
             {error && <Error>{error}</Error>}
         </FormInfo>
