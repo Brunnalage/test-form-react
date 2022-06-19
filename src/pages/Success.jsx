702f2a287168e78ea5370bf0d62cf5ab4a3af313
Brunnalage <brunnalage@gmail.com>
2022-06-18 21:36:47 -0300

    Style: Adding non-underline style to the link

--- src/pages/Success.jsx
+++ src/pages/Success.jsx
@@ -1,10 +1,12 @@
-
 import React from 'react';
 import Box from '../components/Box/Box';
 import { Header } from '../components/Header/Header'; 
 import  Button  from '../components/Button/Button';
 import { MainSuccess } from './Success.styled';
 import { Login } from './Success.styled';
+import {Link} from 'react-router-dom'
+import { StyledLink } from './Success.styled';
+
 
 export function Success(){
 return(
@@ -12,7 +14,7 @@ return(
       <Login>
       <Header type="success" title="Success!"/>      
       <MainSuccess>     
-      <Button type="button" title="Go Back!"/>
+      < StyledLink to="/"><Button type="button" title="Go Back!"/></ StyledLink>
       </MainSuccess>
       </Login>
     </Box>
