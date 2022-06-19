1e079c28cd30aa1eed5f0b4b54fbc0cf4d3cd4ef
Brunnalage <brunnalage@gmail.com>
2022-06-18 21:35:48 -0300

    Style:  Removing underline style from link

--- src/pages/Success.styled.js
+++ src/pages/Success.styled.js
@@ -1,5 +1,6 @@
 
 import styled from 'styled-components';
+import {Link} from 'react-router-dom'
 
 export const Login = styled.div`
 height: 100%;
@@ -18,3 +19,6 @@ grid-area: main;
 
 `;
 
+export const StyledLink = styled(Link)`
+text-decoration: none;
+`;
